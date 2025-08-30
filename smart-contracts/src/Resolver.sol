3b795c54140d01ae9754a39d2fffd881229e5b30
anotherdev <joseluismanco37@gmail.com>
2025-08-30 13:03:57 -0500

    feat: add points mint

--- smart-contracts/src/Resolver.sol
+++ smart-contracts/src/Resolver.sol
@@ -2,14 +2,19 @@
 pragma solidity ^0.8.30;
 import {SchemaResolver} from "eas-contracts/resolver/SchemaResolver.sol";
 import { IEAS, Attestation } from "eas-contracts/IEAS.sol";
+import {PlatoPoints} from "./PlatoPoints.sol";
 
 contract Resolver is SchemaResolver {
 
-    constructor(IEAS eas) SchemaResolver(eas) {
+    PlatoPoints private _platopoints;
+
+    constructor(IEAS eas, PlatoPoints platoPoints) SchemaResolver(eas) {
+      _platopoints = platoPoints;
     }
 
-    function onAttest(Attestation calldata attestation, uint256 /*value*/) internal view override returns (bool) {
-        return true;
+    function onAttest(Attestation calldata attestation, uint256 /*value*/) internal  override returns (bool) {
+      _platopoints.mint(attestation.recipient, 20);
+      return true;
     }
 
     function onRevoke(Attestation calldata /*attestation*/, uint256 /*value*/) internal pure override returns (bool) {
